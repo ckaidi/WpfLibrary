f302021e0322d271336131d33c05c5eabb40bf99
锴迪 陈 <935637782@qq.com>
2022-10-17 18:51:54 +0800

    new

---
+++ Converter/BindingDoubleWithForumlaConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// double根据parameter公式进行求值转化器
+    /// </summary>
+    public class BindingDoubleWithForumlaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double num1 && double.TryParse(parameter.ToString(), out double num2))
+            {
+                return num1 - num2;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
