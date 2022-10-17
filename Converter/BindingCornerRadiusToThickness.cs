09f4016b53614dcdd2b12f066ae636cd7275bd8b
锴迪 陈 <935637782@qq.com>
2022-10-17 14:35:22 +0800

    增加textgrou和combobox group

---
+++ Converter/BindingCornerRadiusToThickness.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// 将CornerRadius绑定到Thickness
+    /// </summary>
+    public class BindingCornerRadiusToThickness : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is CornerRadius corner)
+            {
+                return new Thickness(corner.TopLeft, corner.TopLeft, corner.TopRight, corner.TopRight);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Page.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// 自定义页面,增加了圆角属性
+    /// </summary>
+    public class Page : System.Windows.Controls.Page
+    {
+        /// <summary>
+        /// 圆角半径
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(Page), new PropertyMetadata(new CornerRadius()));
+
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static Page()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Page), new FrameworkPropertyMetadata(typeof(Page)));
+        }
+    }
+}
