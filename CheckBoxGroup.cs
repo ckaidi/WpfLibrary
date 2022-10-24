da9ca65baefb7a6793e64605e7f7387eb644c280
kdj <935637782@QQ.COM>
2022-10-24 09:34:17 +0800

    new

--- CheckBoxGroup.cs
+++ CheckBoxGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,64 @@ namespace WpfLibrary
     /// </summary>
     public class CheckBoxGroup : GroupControl
     {
+        /// <summary>
+        /// ComboBox SelectedItem
+        /// </summary>
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool? IsChecked
+        {
+            get { return (bool?)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool?), typeof(CheckBoxGroup),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    new PropertyChangedCallback(OnCheckedChanged)));
+
+        /// <summary>
+        /// checkbox
+        /// </summary>
+        private CheckBox _checkBox;
+
         static CheckBoxGroup()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBoxGroup), new FrameworkPropertyMetadata(typeof(CheckBoxGroup)));
+        }
+
+        /// <summary>
+        /// Selector的OnSelectedItemChanged
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        public static void OnCheckedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+             var comboBoxGroup = d as CheckBoxGroup;
+            if (comboBoxGroup != null && comboBoxGroup.IsInitialized)
+            {
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _checkBox = (CheckBox)GetTemplateChild("MainCheckBox");
+            _checkBox.Checked += _checkBox_Checked;
+            _checkBox.Unchecked += _checkBox_Unchecked;
+        }
+
+        private void _checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox check)
+                IsChecked = check.IsChecked;
+        }
+
+        private void _checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox check)
+                IsChecked = check.IsChecked;
         }
     }
 }
