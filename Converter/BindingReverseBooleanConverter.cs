94c6dd061fcf3c3eb0084db81541103743822c7d
锴迪 陈 <935637782@qq.com>
2022-10-24 19:46:25 +0800

    new

---
+++ Converter/BindingReverseBooleanConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// bool值反转
+    /// </summary>
+    public class BindingReverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool) && targetType != typeof(bool?))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool) && targetType != typeof(bool?))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+    }
+}
