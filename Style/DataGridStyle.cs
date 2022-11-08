c85b23dbc67ca526c4e1e8d4af6392f61e287047
kdj <935637782@QQ.COM>
2022-11-08 18:42:02 +0800

    new

---
+++ Style/DataGridStyle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfLibrary
+{
+    public partial class DataGridStyle
+    {
+
+        /// <summary>
+        /// 选中同行的时候
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void DataGridCell_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DataGridCell dataGrid)
+            {
+                if (!dataGrid.IsReadOnly)
+                {
+
+                }
+            }
+        }
+    }
+}
