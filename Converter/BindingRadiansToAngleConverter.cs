cd4f3f207c8833dcaa47293be79453fd8a2be556
kdj <935637782@QQ.COM>
2022-11-09 20:50:25 +0800

    new

---
+++ Converter/BindingRadiansToAngleConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// 弧度转角度值转化器
+    /// </summary>
+    public class BindingRadiansToAngleConverter : IValueConverter
+    {
+        static double _factorTo;
+        static double _factorBack;
+
+        static BindingRadiansToAngleConverter()
+        {
+            _factorTo = 180 / Math.PI;
+            _factorBack = Math.PI / 180;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double num)
+            {
+                return num * _factorTo;
+            }
+            return double.NaN;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double num)
+            {
+                return num * _factorBack;
+            }
+            return double.NaN;
+        }
+    }
+}
