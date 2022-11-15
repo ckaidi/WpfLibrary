ac0eb1c627c64315ce0b3fa132d632d8056b693d
锴迪 陈 <935637782@qq.com>
2022-11-15 11:38:08 +0800

    new

---
+++ Converter/BindingStringToDoubleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfLibrary
+{
+    /// <summary>
+    /// 把字符串转为double
+    /// </summary>
+    public class BindingStringToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (double.TryParse(value.ToString(), out double result))
+            {
+                return result;
+            }
+            return value;
+        }
+    }
+}
